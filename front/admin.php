e8bd4093720ea1b45f38e58ad584bc1cd26f023c
macklun <macklun@gmail.com>
2024-01-22 15:59:12 +0800

    完成管理登入功能

--- front/admin.php
+++ front/admin.php
@@ -18,7 +18,28 @@
             $_SESSION['ans']=$a+$b;
             echo $a . " + " .$b . " =";
             ?>
-            <input type="text" name="chk" id="chk"></td>
+            <input type="text" name="ans" id="ans"></td>
     </tr>
 </table>
-<div class="ct"><button>確認</button></div>
+<div class="ct"><button onclick="login('admin')">確認</button></div>
+<script>
+function login(table){
+    $.get('./api/chk_ans.php',{ans:$("#ans").val()},(chk)=>{
+        if(parseInt(chk)==0){
+            alert("對不起，您輸入的驗證碼有誤，請您重新登入")
+        }else{
+            $.post("./api/chk_pw.php",
+                    {table,
+                     acc:$("#acc").val(),
+                     pw:$("#pw").val()},
+                    (res)=>{
+                if(parseInt(res)==0){
+                    alert("帳號或密碼錯誤，請重新輸入")
+                }else{
+                    location.href='back.php?do=admin';
+                }
+            })
+        }
+    })
+}
+</script>
--- front/login.php
+++ front/login.php
@@ -33,7 +33,11 @@ function login(table){
         if(parseInt(chk)==0){
             alert("驗證碼錯誤，請重新輸入")
         }else{
-            $.post("./api/chk_pw.php",{table,acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
+            $.post("./api/chk_pw.php",
+                    {table,
+                     acc:$("#acc").val(),
+                     pw:$("#pw").val()},
+                    (res)=>{
                 if(parseInt(res)==0){
                     alert("帳號或密碼錯誤，請重新輸入")
                 }else{
