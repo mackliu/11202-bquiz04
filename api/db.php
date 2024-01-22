cb285ed0b569d74d7f6b1581cea75e7d28c15b3c
kai <cgk0910@gmail.com>
2024-01-22 10:37:39 +0800

    建立頁尾資料表並新增及顯示頁尾版權

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Taipei");
 session_start();
 class DB{
 
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db15";
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db03";
     protected $pdo;
     protected $table;
     
@@ -142,7 +142,7 @@ function to($url){
 }
 
 
-
+$Bottom=new DB('bottom');
 
 
 ?>
