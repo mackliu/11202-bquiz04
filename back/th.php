5fc759d8f3ae18a970a914f799d0e178ba1c4f65
macklun <macklun@gmail.com>
2024-01-31 13:10:03 +0800

    新增分類資料表

--- back/th.php
+++ back/th.php
@@ -1,11 +1,13 @@
 <h2 class="ct">商品分類</h2>
 <div class="ct">
-    新增大分類 <input type="text" name="big" id="big"> <button>新增</button>
+    新增大分類 <input type="text" name="big" id="big"> 
+              <button>新增</button>
 </div>
 <div class="ct">
     新增中分類 
     <select name="big" id="bigs"></select>
-    <input type="text" name="mid" id="mid"><button>新增</button>
+    <input type="text" name="mid" id="mid">
+    <button>新增</button>
 </div>
 <!-- table.all>(tr.tt>td+td.ct>button*2)+(tr.tt.ct>td*2) -->
 <table class="all">
