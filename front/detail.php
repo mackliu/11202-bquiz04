43a7435eda53c7627ace623afd3f774a80b2ba1b
macklun <macklun@gmail.com>
2024-02-16 15:31:50 +0800

    完成點選我要購買,在購物車中顯示商品

--- front/detail.php
+++ front/detail.php
@@ -53,6 +53,13 @@ $goods=$Goods->find($_GET['id']);
 </div>
 <div class="tt ct">
     購買數量:
-    <input type="number" value="1" style="width:50px;">
-    <img src="./icon/0402.jpg" alt="">
+    <input type="number" id="qt" value="1" style="width:50px;">
+    <img src="./icon/0402.jpg" onclick="buy()">
 </div>
+<script>
+function buy(){
+let id=<?=$_GET['id'];?>;
+let qt=$("#qt").val()
+ location.href=`?do=buycart&id=${id}&qt=${qt}`
+}
+</script>
