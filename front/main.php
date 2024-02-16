a77e779678df6161611931c50825a3bd0564331c
macklun <macklun@gmail.com>
2024-02-16 13:46:07 +0800

    美化商品列表頁

--- front/main.php
+++ front/main.php
@@ -19,19 +19,49 @@ if($type==0){
 
 ?>
 <h2><?=$nav;?></h2>
+<style>
+.item{
+    width:80%;
+    height:160px;
+    background-color: #f4c591;
+    margin:5px auto;
+    display:flex;
 
+}
+.item .img{
+    width:33%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    border:1px solid #999;
+}
+.item .info{
+    width:67%;
+    display:flex;
+    flex-direction: column;
+}
+.info div{
+    border:1px solid #999;
+    border-left:0px;
+    border-top:0px;
+    flex-grow:1;
+}
+.info div:nth-child(1){
+    border-top:1px solid #999;
+}
+</style>
 <?php
 foreach($goods as $good){
 ?>
 <div class='item'>
  <div class="img">
-    <img src="./img/<?=$good['img'];?>" style="width:200px;height:150px">
+    <img src="./img/<?=$good['img'];?>" style="width:80%;height:110px">
  </div>
  <div class="info">
     <div class='ct tt'><?=$good['name'];?></div>
     <div>價錢：<?=$good['price'];?></div>
     <div>規格：<?=$good['spec'];?></div>
-    <div>簡介：<?=$good['intro'];?></div>
+    <div>簡介：<?=mb_substr($good['intro'],0,25);?>...</div>
  </div>   
 </div>
 
