cddff694238b7c1169b040ccbf44df964c0cf53d
macklun <macklun@gmail.com>
2024-02-16 14:46:08 +0800

    建立使用session來紀錄購物車的機制

--- front/buycart.php
+++ front/buycart.php
@@ -5,6 +5,18 @@ to("?do=login");
 
 echo "<h2 class='ct'>{$_SESSION['mem']}的購物車</h2>";
 
-if(!isset($_GET['id'])){
+if(!isset($_SESSION['cart'])){
     echo "<h2 class='ct'>購物車中尚無商品</h2>";
+}else{
+
+    if(isset($_GET['id'])){
+        $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+    }
+    dd($_SESSION['cart']);
+
 }
+
+
+
+
+
